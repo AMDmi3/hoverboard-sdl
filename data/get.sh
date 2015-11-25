9fced02047c6a1a27cc545e9c3278e5e1d0e92db
Dmitry Marakasov <amdmi3@amdmi3.ru>
2015-11-26 01:26:15 +0300

    Add script to download tiles

---
+++ data/get.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+for x in `seq 928 1106`; do
+	for y in `seq -- -1150 -1000`; do
+		echo -n "[$x,$y]: "
+		if [ -e "$x/$y.png" ]; then
+			echo "already downloaded"
+			continue
+		fi
+
+		rm -f current.png
+
+		if wget -q "http://xkcd.com/1608/$x:$y+s.png"; then
+			echo "downloaded"
+			mkdir -p $x
+			mv "$x:$y+s.png" $x/$y.png
+		else
+			echo "doesn't exist"
+		fi
+	done
+done
