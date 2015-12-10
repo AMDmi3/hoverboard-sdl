728e232e8634f7ee542a6ab457b8cda840009af8
Dmitry Marakasov <amdmi3@amdmi3.ru>
2015-12-10 20:35:46 +0300

    Enable smooth texture scaling

--- src/main.cc
+++ src/main.cc
@@ -52,6 +52,8 @@ int main(int /*argc*/, char** /*argv*/) try {
 	SDL2pp::Window window("Hoverboard", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 740, 700, SDL_WINDOW_RESIZABLE);
 	SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+
 	Game game(renderer);
 
 	game.LoadState();
